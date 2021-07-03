469fec61f0db77bece848628fdf63968d204dd92
Sedo-KFM <57474004+Sedo-KFM@users.noreply.github.com>
2021-07-03 23:21:17 +0400

    DataBase connected

--- src/main/java/com/sedo/AxBitTest/controllers/MainController.java
+++ src/main/java/com/sedo/AxBitTest/controllers/MainController.java
@@ -1,5 +1,8 @@
 package com.sedo.AxBitTest.controllers;
 
+import com.sedo.AxBitTest.models.Author;
+import com.sedo.AxBitTest.repo.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +11,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class MainController {
 
+	@Autowired
+	private AuthorRepository authorRepository;
+
 	@GetMapping("/")
-	public String greeting(@RequestParam(name="request", required=false, defaultValue="Request something") String request, Model model) {
-		model.addAttribute("answer", request);
-		return "simpleAnswer";
+	public String greeting(Model model) {
+		Iterable<Author> authors = authorRepository.findAll();
+		model.addAttribute("authors", authors);
+		return "home";
 	}
 
 }
